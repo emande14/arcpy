9b1a13cb4d5f52b0592153e8ccc73dce473dd43c
Edward Anderson <emande14@smumn.edu>
2016-07-26 18:32:07 -0500

    Add files via upload

---
+++ code_block_script.py
@@ -0,0 +1,15 @@
+#MAKE SURE PYTHON RADIO BUTTON IS SELECTED
+
+#PRE-LOGIC SCRIPT CODE/CODEBLOCK
+def myCalc(type):
+    if type == "HAND HOLE":
+        return "Handhole"
+    elif type == "MAN HOLE":
+        return "Manhole"
+    elif type == "Hand hole":
+        return "Handhole"
+    else:
+        return type
+
+#LOWER BOX
+# myCalc( !NAME OF EXISTING FIELD!)
